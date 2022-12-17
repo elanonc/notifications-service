ebd2a9ddec5cf77419e640f0b528e8d25f7b697a
Elano N. Caitano <nunes.elano14@gmail.com>
2022-12-17 16:36:34 -0300

    Criando repositório de notifications

--- src/app/use-cases/send-notification.spec.ts
+++ src/app/use-cases/send-notification.spec.ts
@@ -1,15 +1,26 @@
+import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
+
+const notifications: Notification[] = [];
+
+const notificationsRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationsRepository);
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       content: 'A notification',
       category: 'social',
       recipientId: '123',
     });
 
-    expect(notification).toBeTruthy();
+    console.log(notifications);
+
+    expect(notifications).toHaveLength(1);
   });
 });
--- src/infra/app.controller.ts
+++ src/infra/app.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
-import { AppService } from '../app.service';
+// import { AppService } from '../app.service';
 import { PrismaService } from './prisma.service';
 import { randomUUID } from 'node:crypto';
 import { CreateNotificationBody } from './create-notification-body';
